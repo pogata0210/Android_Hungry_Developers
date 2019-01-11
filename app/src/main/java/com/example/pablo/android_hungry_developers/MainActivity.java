318d59dbff5acd18986a30a044de61b47b81c837
Pablo Ogata <pabloogata@gmail.com>
2019-01-11 11:53:19 -0500

    not finished

--- app/src/main/java/com/example/pablo/android_hungry_developers/MainActivity.java
+++ app/src/main/java/com/example/pablo/android_hungry_developers/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pablo.android_hungry_developers;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spoon spoon1 = new Spoon(1);
+        Spoon spoon2 = new Spoon(2);
+        Spoon spoon3 = new Spoon(3);
+        Spoon spoon4 = new Spoon(4);
+        Spoon spoon5 = new Spoon(5);
+
+        Developer dev1 = new Developer(1, spoon5, spoon1);
+        new Thread(dev1).start();
+        Developer dev2 = new Developer(2, spoon1, spoon2);
+        new Thread(dev2).start();
+        Developer dev3 = new Developer(3, spoon2, spoon3);
+        new Thread(dev3).start();
+        Developer dev4 = new Developer(4, spoon3, spoon4);
+        new Thread(dev4).start();
+        Developer dev5 = new Developer(5, spoon4, spoon5);
+        new Thread(dev5).start();
     }
 }
---
+++ app/src/main/java/com/example/pablo/android_hungry_developers/Spoon.java
@@ -0,0 +1,4 @@
+package com.example.pablo.android_hungry_developers;
+
+public class Spoon {
+}
